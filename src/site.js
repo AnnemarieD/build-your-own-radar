a37b2e56c7c4043835e264c0763f4bec3b897c1c
Annemarie <a.collijn@gmail.com>
2020-09-10 17:36:06 +0200

    feat: added inlineFactory WIP

--- src/site.js
+++ src/site.js
@@ -1,8 +1,8 @@
 require('./common')
-require('./images/logo.png')
+require('./images/749px-AD_Etos_Logo_Positive.jpg')
 require('./images/radar_legend.png')
 require('./gtm.js')
 
-const GoogleSheetInput = require('./util/factory')
+const GoogleSheetInput = require('./util/inlineFactory')
 
 GoogleSheetInput().build()
