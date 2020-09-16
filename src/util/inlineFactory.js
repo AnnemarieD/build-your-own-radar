e9185b047ebb4ff2051e65a31d49de70d56c1037
Annemarie <a.collijn@gmail.com>
2020-09-16 11:23:57 +0200

    chore: replaced dummy data for data from our csv

--- src/util/inlineFactory.js
+++ src/util/inlineFactory.js
@@ -1,23 +1,21 @@
 document.title = 'Etos Tech Radar'
 
 // store data inline directly as a table...
-//const csv = require('../../TechRadar.csv')
-//const rawDataTable = [csv]
 const rawDataTable = [
     [ 'name',                          'ring',   'quadrant',               'isNew', 'description' ],
-    [ 'Grafana',                       'adopt',  'tools',                  'TRUE',  'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Packer',                        'adopt',  'tools',                  'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Apache Kafka',                  'trial',  'tools',                  'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Spring Boot',                   'adopt',  'languages & frameworks', 'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'AngularJS',                     'hold',   'languages & frameworks', 'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Docker',                        'adopt',  'platforms',              'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Pivotal Cloud Foundry',         'trial',  'platforms',              'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'AWS Application Load Balancer', 'assess', 'platforms',              'TRUE',  'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Overambitious API gateways',    'hold',   'platforms',              'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Superficial private cloud',     'hold',   'platforms',              'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Pipelines as code',             'adopt',  'techniques',             'TRUE',  'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Bug bounties',                  'trial',  'techniques',             'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ],
-    [ 'Cloud lift and shift',          'hold',   'techniques',             'FALSE', 'This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href="https://www.thoughtworks.com">anchor links to documentation and referance material</a>.' ]
+    ['WebdriverIO',                 'Adopt',    'tools',                        'FALSE',    'description'],
+    ['UFT',                         'Adopt',    'tools',                        'FALSE',    'description'],
+    ['Leapwork',                    'Assess',   'tools',                        'TRUE',     '<p>Leapwork is used within Gall and AH for testing Oracle applications. A license is available. We have to decide if we want to use this tool at Etos</p>'],
+    ['Selenium',                    'Adopt',    'tools',                        'FALSE',    '<p>A tool to use browser functionality in automated tests</p>'],
+    ['Jenkins',                     'Adopt',    'tools',                        'FALSE',    'Tool where the CICD job pipelines are configured and deployment can be triggered from it'],
+    ['OpenShift',                   'Adopt',    'platforms',                    'FALSE',    'Its a platform where jenkins is been configured'],
+    ['Kubernetes',                  'Adopt',    'techniques',                   'FALSE',    'Tool used to build and implement Jenkins'],
+    ['SonarQube',                   'Adopt',    'tools',                        'FALSE',    'Code quality checker'],
+    ['Java',                        'Adopt',    'languages-and-frameworks',     'FALSE',    '<p>Used as programming language for automated tests for Oracle systems in combination with Selenium.<p>'],
+    ['JavaScript',                  'Adopt',    'languages-and-frameworks',     'FALSE',    '<p>Used as programming language for automated tests for web shop.<p>'],
+    ['Nexus Artifact Repository',   'Adopt',    'tools',                        'FALSE',    'Artifact repository which has backup code and existing packages'],
+    ['Github',                      'Adopt',    'tools',                        'TRUE',     'Code Repository migrated from bitbucket'],
+    ['Bitbucket',                   'Adopt',    'tools',                        'FALSE',    'Code Repository']
 ]
 
 // which will need to be converted to an array of objects...
