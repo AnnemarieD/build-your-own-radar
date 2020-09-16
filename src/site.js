53ccd5665320026d3b9a3f8e0eddcb66867a029b
Annemarie <a.collijn@gmail.com>
2020-09-16 12:10:23 +0200

    chore: added jquery-csv to handle converting csv

--- src/site.js
+++ src/site.js
@@ -3,6 +3,6 @@ require('./images/749px-AD_Etos_Logo_Positive.jpg')
 require('./images/radar_legend.png')
 require('./gtm.js')
 
-const GoogleSheetInput = require('./util/inlineFactory')
+const GoogleSheetInput = require('./util/csvFactory')
 
 GoogleSheetInput().build()
